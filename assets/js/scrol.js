26fa08ea3a33ad84582f72102388f72bd2925c83
Abdur Rehman Mughal <122609108+AbdurRehmany@users.noreply.github.com>
2023-02-14 08:14:44 -0800

    Add files via upload

---
+++ assets/js/scrol.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal(".customcard", { delay: 100, easing: "ease-out", interval: 90, scale: 1.2 });
+
+
+ScrollReveal().reveal(".direction", { delay: 900, easing: "ease-out", interval: 200, scale: 1.2, distance: '600px', origin: 'right' });
+
+ScrollReveal().reveal(".btomanimate", { delay: 900, easing: "ease-out", interval: 200, scale: 1.2, distance: '600px', origin: 'bottom' });
---
+++ assets/js/scrolingeffect.js
@@ -0,0 +1,18 @@
+// window.addEventListener('scroll', reveal);
+
+// function reveal() {
+//     var reveal = document.querySelector('.reveal');
+
+//     for (var i = 0; i < reveal.length; i++) {
+//         var windowheight = window.innerHeight;
+//         var revealtop = reveal[i].getBondingClientRect().top;
+//         var revealpoint = 150;
+
+//         if (revealtop < windowheight - revealpoint) {
+//             reveal[i].ClassList.add('active');
+
+//         } else {
+//             reveal[i].ClassList.remove('active');
+//         }
+//     }
+// }
